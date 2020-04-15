12935cfc77cd9267f79cde50c3e0ab7d0f92b8cb
IvanNextToJunior <Vanwan@bk.ru>
2020-04-15 20:58:30 +0300

    Project

---
+++ TimeFormatter/Timeformatter.swift
@@ -0,0 +1,59 @@
+//
+//  Timeformatter.swift
+//  TimeFormatter
+//
+//  Created by Ivan on 09.02.2020.
+//  Copyright © 2020 Ivan Maslov. All rights reserved.
+//
+
+import Foundation
+class TimeFormatter{
+    
+    private var seconds = TimeInterval()
+    //Метод, преобразующий время в нужный формат
+    private func timeFormatter (_ : TimeInterval) -> DateComponentsFormatter{
+        let formatter = DateComponentsFormatter()
+        // Длительность будет отображаться как 1:00:00
+        if seconds > 59.59{
+            formatter.allowedUnits = [.hour, .minute, .second]
+        }
+            
+        else{
+            formatter.allowedUnits = [.minute, .second]
+        }
+        
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        
+        
+        return formatter
+    }
+    
+    //Метод, передающий информацию о длительности записи в приложение
+    func convertTimeToString(_ number: Int64?) -> String?{
+        
+       
+        
+        if let unwrappedNumber = number{
+            
+            seconds = TimeInterval(integerLiteral: unwrappedNumber)
+            
+            print(seconds)
+        }
+        //Здесь мы преобразуем время в нужный нам формат
+        let formatter = timeFormatter(seconds)
+        //А здесь мы преобразуем его в строку
+        let timeString = formatter.string(from: seconds)
+       
+        return timeString
+    
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ TimeFormatter/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  TimeFormatter
+//
+//  Created by Ivan on 09.02.2020.
+//  Copyright © 2020 Ivan Maslov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var durationLabel: UILabel!
+    
+    
+    
+    
+    
+  
+    //Число, которое мы будем преобразовывать в секунды
+    let x: Int64? = 55
+   
+    //Эта переменная отсчитывает количество секунд, отыгранных от начала записи
+    var beginning = TimeInterval()
+  
+    //Это длительность нашей записи и подсчет времени, оставшегося до ее окончания
+    private var end = TimeInterval()
+    
+    //Это длительность в формате Int64, для преобразования в текст с помощью TimeFormatter
+    var end64 = Int64()
+    
+    //MARK: time setup
+    //Мне известно, что возвращаемый DateFormatter String опциональный, поэтому я и создал переменную, в которую мы будем передавать результат форматирования
+    var time: String?
+    
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if let unwrappedValue = x{
+            end = TimeInterval(integerLiteral: unwrappedValue)
+        end64 = Int64(end)
+            
+            
+        }
+        durationLabel.text = time
+        
+        beginning = 0.0
+        
+        //MARK: Label initialization
+
+        setTimer()
+        
+      
+    }
+
+    func setTimer (){
+         //Вот кто отвечает за смену секунд на экране
+               let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeCounter(_:)), userInfo: nil, repeats: true)
+                      
+                      if end == 0.0{
+                          timer.invalidate()
+                       
+                      }
+         
+               }
+       
+     //Метод подсчета времени
+    @objc func timeCounter ( _ : TimeInterval) -> TimeInterval{
+        if end > 0.0{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+                //Здесь мы отображаем секунду в Int64
+                self.end64 = Int64(self.end)
+                //И преобразуем новое время в String
+                self.time = TimeFormatter().convertTimeToString(self.end64)
+                
+                self.durationLabel.text = self.time
+            
+                //Затем мы вновь считаем, сколько секунд записи нам осталось прослушать, и все начинается по новой
+                self.end -= 1.0
+                
+                self.beginning += 1.0
+            }
+            }
+   return end
+    }
+}
+
+    
+    
+
