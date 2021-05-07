0d0f18eecb88df3f00b3dea03e85e3008dab50ac
IvanNextToJunior <Vanwan@bk.ru>
2021-05-07 21:47:48 +0300

    Update Timeformatter.swift

--- TimeFormatter/Timeformatter.swift
+++ TimeFormatter/Timeformatter.swift
@@ -7,20 +7,21 @@
 //
 
 import Foundation
+
 class TimeFormatter{
     
     private var seconds = TimeInterval()
-   
+    
     //Метод, преобразующий время в нужный формат
     private func timeFormatter (_ : TimeInterval) -> DateComponentsFormatter{
-       
+        
         let formatter = DateComponentsFormatter()
-       
+        
         // Длительность будет отображаться как 1:00:00
         if seconds > 59.59{
             formatter.allowedUnits = [.hour, .minute, .second]
         }
-            
+        
         else {
             formatter.allowedUnits = [.minute, .second]
         }
@@ -39,14 +40,13 @@ class TimeFormatter{
             seconds = TimeInterval(integerLiteral: unwrappedNumber)
             print(seconds)
         }
-       
+        
         //Здесь мы преобразуем время в нужный нам формат
         let formatter = timeFormatter(seconds)
         //А здесь мы преобразуем его в строку
         let timeString = formatter.string(from: seconds)
-       
         return timeString
-    
+        
         
     }
 }
